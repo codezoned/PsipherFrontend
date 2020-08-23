aa6f51602e1a6a772994c97dcd75bc06e50616ef
Rahul Krishnan <rahulkrishnan221@gmail.com>
2020-08-23 22:24:54 +0530

    Added dashboard base

---
+++ src/components/styled/SearchInputWrapper.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import { Input } from 'antd';
+
+const SearchWrapper = styled(Input)`
+  padding-right: 20px;
+`;
+
+export default SearchWrapper;
--- src/containers/DashboardContainer.js
+++ src/containers/DashboardContainer.js
@@ -1,14 +1,15 @@
 import * as React from 'react';
+import SearchWrapper from '../components/styled/SearchInputWrapper'
 import { Auth } from 'aws-amplify';
-import { RouteComponentProps, withRouter } from 'react-router';
-import { Layout, Menu, Icon, notification } from 'antd';
+import { withRouter } from 'react-router';
+import { Layout, Menu, Icon, notification, Button, List, Card } from 'antd';
+import {PlusOutlined, SearchOutlined, PlusCircleFilled} from '@ant-design/icons'
 
 /** App Theme */
 import { colors } from '../themes/Colors';
 
 /** App Constatns */
 import { AUTH_USER_TOKEN_KEY } from '../utils/constants';
-import { ClickParam } from 'antd/lib/menu';
 
 const DashBoardContainer = props => {
     const [collapsed, setCollapsed] = React.useState(false);
@@ -24,9 +25,46 @@ const DashBoardContainer = props => {
     }
   };
 
+//Adding this temporary data till we fetch from backend
+    const data = [
+        {
+            domain: 'google.co.in',
+            email: ['abc@google.com', "test@google.com"]
+        },
+        {
+            domain: 'facebook.com',
+            email: ['abc@google.com', "test@google.com"]
+        },
+        {
+            domain: 'twitter.com',
+            email: ['abc@gmail.com', "test@gmail.com"]
+        },
+        {
+            domain: 'instagram.com',
+            email: ['abc@google.com', "test@google.com"]
+        },
+        {
+            domain: 'amazon.in',
+            email: ['abc@amazon.com', "xyz@amazon.com"]
+        },
+        {
+            domain: 'flipkart.com',
+            email: ['abc@flipkart.com', "test@flipkart.com"]
+        },
+    ];
+
   return (
-    <Layout className="cover" id="app-header">
-      <Layout.Sider className="cover" trigger={null} collapsible collapsed={collapsed}>
+    <Layout className="cover" id="app-header" style={{ minHeight: '100vh' }}>
+      <Layout.Sider className="cover" trigger={null} collapsible collapsed={collapsed} width={250} >
+          <div style={{display:"flex", marginTop:20, marginBottom: 20}}>
+              <Icon
+                  style={{color:colors.white, marginTop:10, marginLeft:25, marginRight:10}}
+                  className="trigger"
+                  onClick={() => setCollapsed(!collapsed)}
+                  type={collapsed ? 'menu-unfold' : 'menu-fold'}
+              />
+                  <SearchWrapper prefix={<SearchOutlined/>}/>
+          </div>
         <div className="logo" />
         <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
           <Menu.Item key="1">
@@ -43,26 +81,37 @@ const DashBoardContainer = props => {
           </Menu.Item>
         </Menu>
       </Layout.Sider>
-      <Layout>
+      <Layout style={{background: colors.white}}>
         <Layout.Header style={{ background: colors.white, padding: 0 }}>
-          <Icon
-            className="trigger"
-            onClick={() => setCollapsed(!collapsed)}
-            type={collapsed ? 'menu-unfold' : 'menu-fold'}
-          />
+
+          <span style={{paddingRight: 50}}> </span>
+            <Button type="primary" >
+                <PlusOutlined />
+                Add New
+            </Button>
         </Layout.Header>
-        <Layout.Content
-          style={{
-            margin: '24px 16px',
-            padding: 24,
-            background: colors.white,
-            minHeight: 280
-          }}
-        >
-          <div className="text-center">
-            <h1>Hello world</h1>
-          </div>
-        </Layout.Content>
+          <List
+              grid={{
+                  gutter: 16,
+                  xs: 1,
+                  sm: 2,
+                  md: 4,
+                  lg: 4,
+                  xl: 6,
+                  xxl: 3,
+              }}
+              dataSource={data}
+              renderItem={item => (
+                  <List.Item>
+                      <Card title={item.domain} actions={[<PlusCircleFilled />]}>{<List dataSource={item.email}
+                                                                                       renderItem={emails => (
+                                                                                           <List.Item>
+                                                                                               {emails}
+                                                                                           </List.Item>)}/>}
+                      </Card>
+                  </List.Item>
+              )}
+          />
       </Layout>
     </Layout>
   );
